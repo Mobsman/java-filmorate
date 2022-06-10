e8dfef73eaa7b13797e8d9894be6d9b57a8e4785
Azat Bay <mobsman@yandex.ru>
2022-06-10 20:57:29 +0300

    commit

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,40 @@
+
+CREATE TABLE Film (
+                      id INTEGER NOT NULL PRIMARY KEY,
+                      name VARCHAR(45) NOT NULL,
+                      description VARCHAR(600) NOT NULL,
+                      duration INTEGER NOT NULL,
+                      releaseDate TIMESTAMP NOT NULL,
+                      rating_id  INT REFERENCES Rating(id));
+
+CREATE TABLE Rating (
+                        id INTEGER NOT NULL PRIMARY KEY,
+                        name VARCHAR(6) NOT NULL);
+
+CREATE TABLE Genre_film (
+                            film_id INT REFERENCES Film(id),
+                            rating_id INT REFERENCES Rating(id));
+
+CREATE TABLE Genre (
+                       id INTEGER NOT NULL PRIMARY KEY,
+                       name VARCHAR(15) NOT NULL);
+
+CREATE TABLE Users (
+                      id INTEGER NOT NULL PRIMARY KEY,
+                      email VARCHAR(45) NOT NULL,
+                      login VARCHAR(45) NOT NULL,
+                      name VARCHAR(45) NOT NULL,
+                      birthday TIMESTAMP NOT NULL);
+
+CREATE TABLE Likes (
+                      film_id INT REFERENCES Film(id),
+                      users_id INT REFERENCES Users(id));
+
+
+CREATE TABLE Friend (
+                        from_requested_id INT REFERENCES Users(id),
+                        to_request_id INT REFERENCES Users(id),
+                        is_confirmed BOOLEAN NOT NULL);
+
+
+
